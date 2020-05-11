450909f16b083e8822dbc92775225730a37b866d
Santiago Mariani <santiagomariani2@gmail.com>
2020-05-11 18:13:55 -0300

    agrego componente para la pagina sign in

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+//#import PrivateRoute from './PrivateRoute'
+import SignInPage from './SignInPage'
+import {Switch,Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and saave to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Switch>
+    <Route exact path="/">
+      <SignInPage></SignInPage>
+    </Route> 
+    {/*
+    <Route>
+      <NoMatchPage></NoMatchPage>
+    </Route>
+    */
+    }
+  </Switch>
   );
 }
 
