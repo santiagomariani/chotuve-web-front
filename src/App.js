cb3458b6876b5467f4e9a4746c6de4c853414f4e
Santiago Mariani <santiagomariani2@gmail.com>
2020-05-11 23:08:43 -0300

    algunas modificaciones

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 //#import PrivateRoute from './PrivateRoute'
 import SignInPage from './SignInPage'
+import NoMatchPage from './NoMatchPage'
 import {Switch,Route} from "react-router-dom";
 
 function App() {
@@ -10,12 +11,9 @@ function App() {
     <Route exact path="/">
       <SignInPage></SignInPage>
     </Route> 
-    {/*
     <Route>
       <NoMatchPage></NoMatchPage>
     </Route>
-    */
-    }
   </Switch>
   );
 }
