c07e9edf2e33c4eb7069fbee0c2e5e670632d753
Santiago Mariani <santiagomariani2@gmail.com>
2020-05-11 23:57:45 -0300

    agrego un template para el dashboard, esto desp. se modifica bien acorde a lo que necesitamos

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 //#import PrivateRoute from './PrivateRoute'
 import SignInPage from './SignInPage'
 import NoMatchPage from './NoMatchPage'
+import DashboardPage from './DashboardPage'
 import {Switch,Route} from "react-router-dom";
 
 function App() {
   return (
   <Switch>
+    <Route path="/dashboard">
+      <DashboardPage></DashboardPage>
+    </Route>
     <Route exact path="/">
       <SignInPage></SignInPage>
     </Route> 
