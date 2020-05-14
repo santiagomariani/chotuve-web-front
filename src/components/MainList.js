42f81ac630a8e074747d529994067aea13cc078e
Santiago Mariani <santiagomariani2@gmail.com>
2020-05-14 19:25:35 -0300

    creo nuevo componente para el link en el menu

--- src/components/MainList.js
+++ src/components/MainList.js
@@ -11,49 +11,43 @@ import LayersIcon from '@material-ui/icons/Layers';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-    estiloLink: {
-      textDecoration: 'none',
-    }
-}));
+import MenuLink from './MenuLink';
 
 export default function MainList () {
-    const  classes = useStyles();
     return (
         <div>
-        <Link href="/dashboard" className={classes.estiloLink}>
+        <MenuLink href="/dashboard">
           <ListItem button>
             <ListItemIcon>
               <DashboardIcon />
             </ListItemIcon>
             <ListItemText primary="Dashboard" />
           </ListItem>
-        </Link>
-        <Link href="/resources">
+        </MenuLink>
+        <MenuLink href="/resources">
           <ListItem button>
             <ListItemIcon>
               <VideoLibraryIcon />
             </ListItemIcon>
             <ListItemText primary="Resources" />
           </ListItem>
-        </Link>
-        <Link href="/users">
+        </MenuLink>
+        <MenuLink href="/users">
           <ListItem button>
             <ListItemIcon>
               <PeopleIcon />
             </ListItemIcon>
             <ListItemText primary="Users" />
           </ListItem>
-        </Link>
-        <Link href="/app-servers">
+        </MenuLink>
+        <MenuLink href="/app-servers">
           <ListItem button>
             <ListItemIcon>
               <PhoneIphoneIcon />
             </ListItemIcon>
             <ListItemText primary="App servers" />
           </ListItem>
-        </Link>
+        </MenuLink>
       </div>
     )
   }
---
+++ src/components/MenuLink.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    estiloLink: {
+      textDecoration: 'none',
+      color: 'white'
+    }
+}));
+
+export default function MenuLink (props) {
+    const  classes = useStyles();
+    return (
+        <Link className={classes.estiloLink} {...props} />
+    )
+  }
